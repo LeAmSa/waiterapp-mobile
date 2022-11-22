5bc193fcbccbca0f82b74f0de4008eaae8164efe
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-21 21:55:49 -0300

    Add custom native-base theme

---
+++ src/theme/index.ts
@@ -0,0 +1,35 @@
+import { extendTheme } from "native-base";
+
+export const THEME = extendTheme({
+  colors: {
+    red: {
+      light: "#FFABAD",
+      default: "#D73035",
+      dark: "#8A1114",
+    },
+
+    white: "#FFFFFF",
+
+    gray: {
+      100: "#F2F2F2",
+      200: "#CCCCCC",
+      300: "#999999",
+      400: "#666666",
+      500: "#333333",
+    },
+
+    black: "#000000",
+  },
+
+  fonts: {
+    regular: "Inter_400Regular",
+    semi_bold: "Inter_600SemiBold",
+    bold: "Inter_700Bold",
+  },
+
+  fontSizes: {
+    sm: 14,
+    md: 16,
+    lg: 24,
+  },
+});
