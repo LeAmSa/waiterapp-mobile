080974fa9362bb5eae5c61e265755fff6ec2000f
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-22 16:57:19 -0300

    Update Product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,35 +1,58 @@
-import { HStack, VStack, Image, Text } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { HStack, VStack, Image, Text, Box } from "native-base";
+
+import { PlusCircle } from "phosphor-react-native";
 
 import { formatCurrency } from "../utils/formatCurrency";
 
-interface ProductProps {
-  imagePath: string;
-  name: string;
-  description: string;
-  price: number;
+import { ProductProps } from "../@types/product";
+
+interface CustomProductProps {
+  product: ProductProps;
+  setIsModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setSelectedProduct: React.Dispatch<React.SetStateAction<ProductProps | null>>;
 }
 
-export function Product({ imagePath, name, description, price }: ProductProps) {
-  return (
-    <HStack alignItems="center">
-      <Image
-        // source={{ uri: `http://192.168.15.5:3333/uploads/${imagePath}` }}
-        alt={name}
-        w="40"
-        rounded="lg"
-      />
+export function Product({
+  product,
+  setIsModalVisible,
+  setSelectedProduct,
+}: CustomProductProps) {
+  function handleOpenProductDetailsModal() {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
 
-      <VStack ml="4" flex={1}>
-        <Text color="gray.500" fontFamily="semi_bold">
-          {name}
-        </Text>
-        <Text color="gray.300" fontSize="xs" my="1">
-          {description}
-        </Text>
-        <Text fontFamily="semi_bold" color="gray.500" fontSize="xs">
-          {formatCurrency(price)}
-        </Text>
-      </VStack>
-    </HStack>
+  return (
+    <Box flex={1}>
+      <TouchableOpacity onPress={() => handleOpenProductDetailsModal()}>
+        <HStack alignItems="center">
+          <Image
+            // source={{ uri: `http://192.168.15.5:3333/uploads/${imagePath}` }}
+            source={{ uri: "https://wallpaperaccess.com/full/317501.jpg" }}
+            alt={product.name}
+            size={150}
+            rounded="lg"
+          />
+
+          <VStack ml="4" flex={1}>
+            <Text color="gray.500" fontFamily="semi_bold">
+              {product.name}
+            </Text>
+            <Text color="gray.300" fontSize="xs" my="1">
+              {product.description}
+            </Text>
+            <Text fontFamily="semi_bold" color="gray.500" fontSize="xs">
+              {formatCurrency(product.price)}
+            </Text>
+          </VStack>
+        </HStack>
+      </TouchableOpacity>
+      <Box position="absolute" bottom="0" right="0">
+        <TouchableOpacity onPress={() => alert("Item adicionado!")}>
+          <PlusCircle color="#D73035" />
+        </TouchableOpacity>
+      </Box>
+    </Box>
   );
 }
