b75d07cff081e18188bf952ac8a0e3f396a69353
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-22 16:55:40 -0300

    Add modal and selected table conditionals

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 import { VStack, HStack, Center } from "native-base";
 import { Button } from "./Button";
 import { Categories } from "./Categories";
 
 import { Header } from "./Header";
 import { Menu } from "./Menu";
+import { NewOrderModal } from "./NewOrderModal";
 
 export function Main() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTable, setSelectedTable] = useState("");
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
+  function handleCancelOrder() {
+    setSelectedTable("");
+  }
+
   return (
     <VStack flex={1} bgColor="gray.100" safeArea>
-      <Header />
+      <Header selectedTable={selectedTable} onCancelOrder={handleCancelOrder} />
 
       <HStack h="80px">
         <Categories />
@@ -18,9 +32,17 @@ export function Main() {
         <Menu />
       </VStack>
 
-      <Center minH="110px" bgColor="white" px="6">
-        <Button onPress={() => alert("Clicadassasso!")}>Novo Pedido</Button>
-      </Center>
+      {!selectedTable && (
+        <Center minH="110px" bgColor="white" px="6">
+          <Button onPress={() => setShowModal(true)}>Novo Pedido</Button>
+        </Center>
+      )}
+
+      <NewOrderModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        onSave={handleSaveTable}
+      />
     </VStack>
   );
 }
