4ea6a4e34f6942e808a0b798fbb21425efd650ef
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-23 21:56:06 -0300

    Add Cart Item function

--- src/components/ProductDetailsModal.tsx
+++ src/components/ProductDetailsModal.tsx
@@ -1,18 +1,35 @@
-import { Modal } from "native-base";
+import { ImageBackground } from "react-native";
+
+import { Modal, Text, VStack, HStack } from "native-base";
 
 import { ProductProps } from "../@types/product";
+
+import { Ingredient } from "./Ingredient";
+import { Button } from "./Button";
+import { formatCurrency } from "../utils/formatCurrency";
 
 interface ProductDetailsModalProps {
   product: ProductProps | null;
   isModalVisible: boolean;
   setIsModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  onAddToCart: (product: ProductProps) => void;
 }
 
 export function ProductDetailsModal({
   product,
   isModalVisible,
   setIsModalVisible,
+  onAddToCart,
 }: ProductDetailsModalProps) {
+  if (!product) {
+    return null;
+  }
+
+  function handleAddToCart(product: ProductProps) {
+    onAddToCart(product);
+    setIsModalVisible(false);
+  }
+
   return (
     <Modal
       size="full"
@@ -21,12 +38,58 @@ export function ProductDetailsModal({
       animationPreset="slide"
     >
       <Modal.Content bgColor="white">
-        <Modal.CloseButton />
-        <Modal.Header></Modal.Header>
+        <Modal.CloseButton
+          bgColor="black.opacity"
+          borderRadius="full"
+          _icon={{ color: "white" }}
+        />
+        <Modal.Header p="0">
+          <ImageBackground
+            source={{ uri: "https://wallpaperaccess.com/full/317501.jpg" }}
+            style={{ width: "100%", height: 200 }}
+          />
+        </Modal.Header>
 
-        <Modal.Body>{product?.name}</Modal.Body>
+        <Modal.Body bgColor="gray.100">
+          <Text fontFamily="semi_bold" fontSize="lg" color="gray.500">
+            {product.name}
+          </Text>
+          <Text color="gray.400" mt="2">
+            {product.description}
+          </Text>
 
-        <Modal.Footer></Modal.Footer>
+          {product.ingredients?.length > 0 && (
+            <VStack mt="8">
+              <Text mb="4" fontFamily="semi_bold" color="gray.400">
+                Ingredientes
+              </Text>
+
+              <VStack>
+                {product.ingredients?.map((ingredient) => (
+                  <Ingredient
+                    key={ingredient._id}
+                    icon={ingredient.icon}
+                    name={ingredient.name}
+                  />
+                ))}
+              </VStack>
+            </VStack>
+          )}
+        </Modal.Body>
+
+        <Modal.Footer>
+          <HStack w="full" alignItems="center" justifyContent="space-between">
+            <VStack>
+              <Text color="gray.400">Preço</Text>
+              <Text fontFamily="semi_bold" fontSize="20" color="gray.500">
+                {formatCurrency(product.price)}
+              </Text>
+            </VStack>
+            <Button maxW="200" onPress={() => handleAddToCart(product)}>
+              Adicionar ao pedido
+            </Button>
+          </HStack>
+        </Modal.Footer>
       </Modal.Content>
     </Modal>
   );
