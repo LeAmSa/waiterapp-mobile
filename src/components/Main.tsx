40237d8dadc77ec4800f28a662c7d01006badcfb
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-23 21:54:47 -0300

    Add Cart functions

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 
-import { VStack, HStack, Center } from "native-base";
+import { VStack, HStack, Center, Box } from "native-base";
 import { Button } from "./Button";
 import { Categories } from "./Categories";
 
 import { Header } from "./Header";
 import { Menu } from "./Menu";
 import { NewOrderModal } from "./NewOrderModal";
+import { Cart } from "./Cart";
+
+import { CartItemProps } from "../@types/cartItem";
+import { ProductProps } from "../@types/product";
+
+import { products } from "../mocks/products";
 
 export function Main() {
   const [showModal, setShowModal] = useState(false);
   const [selectedTable, setSelectedTable] = useState("");
+  const [cartItems, setCartItems] = useState<CartItemProps[]>([]);
 
   function handleSaveTable(table: string) {
     setSelectedTable(table);
@@ -18,6 +25,61 @@ export function Main() {
 
   function handleCancelOrder() {
     setSelectedTable("");
+    setCartItems([]);
+  }
+
+  function handleAddToCart(product: ProductProps) {
+    if (!selectedTable) {
+      setShowModal(true);
+    }
+
+    setCartItems((prev) => {
+      //get index of added product in cart list
+      const itemIndex = prev.findIndex(
+        (cartItem) => cartItem.product._id === product._id
+      );
+
+      //itemIndex < 0 means added product is not in cart list
+      if (itemIndex < 0) {
+        return [...prev, { quantity: 1, product }];
+      }
+
+      const newCartItems = [...prev];
+      const item = newCartItems[itemIndex];
+
+      newCartItems[itemIndex] = {
+        ...item,
+        quantity: item.quantity + 1,
+      };
+
+      return newCartItems;
+    });
+  }
+
+  function handleDecrementItemFromCart(product: ProductProps) {
+    setCartItems((prev) => {
+      //get index of added product in cart list
+      const itemIndex = prev.findIndex(
+        (cartItem) => cartItem.product._id === product._id
+      );
+
+      const item = prev[itemIndex];
+      const newCartItems = [...prev];
+
+      //if product quantity === 1, then remove from cart list
+      if (item.quantity === 1) {
+        newCartItems.splice(itemIndex, 1);
+
+        return newCartItems;
+      }
+
+      newCartItems[itemIndex] = {
+        ...item,
+        quantity: item.quantity - 1,
+      };
+
+      return newCartItems;
+    });
   }
 
   return (
@@ -29,13 +91,21 @@ export function Main() {
       </HStack>
 
       <VStack flex={1} px="6">
-        <Menu />
+        <Menu onAddToCart={handleAddToCart} />
       </VStack>
 
-      {!selectedTable && (
+      {!selectedTable ? (
         <Center minH="110px" bgColor="white" px="6">
           <Button onPress={() => setShowModal(true)}>Novo Pedido</Button>
         </Center>
+      ) : (
+        <Box bgColor="white" minH="110px" px="6">
+          <Cart
+            cartItems={cartItems}
+            onAddToCart={handleAddToCart}
+            onDecrementItemFromCart={handleDecrementItemFromCart}
+          />
+        </Box>
       )}
 
       <NewOrderModal
