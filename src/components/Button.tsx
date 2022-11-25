70d2e03509f950511230304c83f843cbc40689fb
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-24 21:16:11 -0300

    Add isOkConfirm property

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,19 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
 
 interface ButtonProps extends IButtonProps {
   children: React.ReactNode;
+  isOkConfirm?: boolean;
 }
 
-export function Button({ children, disabled, ...rest }: ButtonProps) {
+export function Button({
+  children,
+  disabled,
+  isOkConfirm,
+  ...rest
+}: ButtonProps) {
   return (
     <ButtonNativeBase
-      w="full"
-      bgColor={disabled ? "gray.300" : "red.default"}
+      w={isOkConfirm ? "20" : "full"}
+      bgColor={disabled ? "gray.300" : isOkConfirm ? "white" : "red.default"}
       rounded="3xl"
       color="white"
       py="14px"
@@ -16,7 +22,10 @@ export function Button({ children, disabled, ...rest }: ButtonProps) {
       disabled={disabled}
       {...rest}
     >
-      <Text fontFamily="semi_bold" color="white">
+      <Text
+        fontFamily="semi_bold"
+        color={isOkConfirm ? "red.default" : "white"}
+      >
         {children}
       </Text>
     </ButtonNativeBase>
