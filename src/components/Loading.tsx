ce33f5da2dbda131efdb735c86a5d630d2de2b94
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-28 21:45:43 -0300

    Change height full to flex 1 in center view

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ import { Center } from "native-base";
 
 export function Loading() {
   return (
-    <Center w="full" h="full">
+    <Center w="full" flex="1">
       <ActivityIndicator color="#D73035" />
     </Center>
   );
